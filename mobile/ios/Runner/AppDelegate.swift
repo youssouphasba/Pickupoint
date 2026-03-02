19ded5869cc09419ffea6c33b26fbe6cdb4206fa
youssouphasba <youssouphasba@gmail.com>
2026-03-02 07:12:26 +0100

    remove ios api key

--- mobile/ios/Runner/AppDelegate.swift
+++ mobile/ios/Runner/AppDelegate.swift
@@ -8,8 +8,9 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Fournir la clé API Google Maps pour iOS
-    GMSServices.provideAPIKey("***REMOVED_IOS_KEY***")
+    // Fournir la clé API Google Maps pour iOS (Ne pas builder avec la clé en dur sur git)
+    let googleMapsApiKey = "VOTRE_CLE_IOS_ICI"
+    GMSServices.provideAPIKey(googleMapsApiKey)
     
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
